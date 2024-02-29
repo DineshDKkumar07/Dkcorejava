2ff3663256eea41df36e5a1b1acc4df456bd0251
DineshDKkumar07 <dineshdkssm@gmail.com>
2024-02-29 17:02:57 +0530

    adding one assignment

---
+++ src/Assignments_Corejava/Assignment_2.java
@@ -0,0 +1,17 @@
+package Assignments_Corejava;
+
+public class Assignment_2 {
+
+			public static void main(String[] args) {
+				
+			System.out.println("Hi");
+			System.out.println("Dinesh");
+			System.out.println("Kumar");
+
+			}
+
+		
+
+	}
+
+
