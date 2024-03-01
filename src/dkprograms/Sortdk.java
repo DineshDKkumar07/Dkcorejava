d1cfc766403f06ee2621bc77ecbb6ec56db099fe
DineshDKkumar07 <dineshdkssm@gmail.com>
2024-03-01 20:29:42 +0530

    adding program

---
+++ src/dkprograms/Sortdk.java
@@ -0,0 +1,22 @@
+package dkprograms;
+
+import java.util.Arrays;
+
+public class Sortdk {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int[] values= {1,7,2,4,0,4,2};
+		
+		Arrays.sort(values);
+		
+		
+		System.out.println(Arrays.toString(values));
+		
+		
+		
+
+}
+}
