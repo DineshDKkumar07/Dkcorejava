a4a651104be723f5396a3e9b86622cb6677bf674
DineshDKkumar07 <dineshdkssm@gmail.com>
2024-03-01 12:26:28 +0530

    adding assignment 54

---
+++ src/Assignments_Corejava/Assignment_54.java
@@ -0,0 +1,20 @@
+package Assignments_Corejava;
+
+public class Assignment_54 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=40;
+		int b=50;
+		
+		a=a+b; //90   
+		b=a-b;  //-->90-50=40
+		a=a-b;  //-->90-40=50
+		
+		System.out.println("a is " + a);
+		System.out.println("b is " + b);
+		
+	}
+
+}
