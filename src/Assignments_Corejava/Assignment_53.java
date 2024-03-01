f4340bcd195d6cfd47c54090570afd97fb0c7e6b
DineshDKkumar07 <dineshdkssm@gmail.com>
2024-03-01 12:15:16 +0530

    adding assignment_53

---
+++ src/Assignments_Corejava/Assignment_53.java
@@ -0,0 +1,26 @@
+package Assignments_Corejava;
+
+public class Assignment_53 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a=30;
+		int b=60;
+		
+		int c;
+		
+		c=a;
+		
+		a=b;
+		
+		b=c;
+		
+		System.out.println("a is " + a);
+		
+		System.out.println("b is " + b);
+		
+	
+	}
+
+}
