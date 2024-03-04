37b2e1099f1dae01ee1d4132199e8a310c363d9a
DineshDKkumar07 <dineshdkssm@gmail.com>
2024-03-04 18:46:57 +0530

    adding code

---
+++ src/Arrays/Swap.java
@@ -0,0 +1,25 @@
+package Arrays;
+
+public class Swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String a="Ravi";
+		
+		String b="Das";
+		
+		String temp ="";
+		
+		temp="ravi";
+		
+		a=b;
+		
+		b=temp;
+		
+		System.out.print(a + " "  + b);
+		
+		
+	}
+
+}
