399097d84cef407523170ffa175387a5f82ceb12
DineshDKkumar07 <dineshdkssm@gmail.com>
2024-03-05 23:38:02 +0530

    modified codes

--- src/dkprograms/Circledk.java
+++ src/dkprograms/Circledk.java
@@ -3,38 +3,31 @@ package dkprograms;
 import java.util.Scanner;
 
 public class Circledk {
-	 static int r;
-	static  double pi;
-	 
-	 /*void mul() {
-		 
-		 double area_of_circle=pi*r*r;
-		 
-		 System.out.println(area_of_circle);
-	 }*/
 	
+	 static int r;                                //Non static global variables cannot initialize inside static methods
+	 static double pi;                           //static global variables can be initialize inside both static and non static methods
+ 
+	 void mul() {
+		
+		double area_of_cirlce=pi*r*r;
+		System.out.println(area_of_cirlce);
+		
+	}
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		//int r;
+		//double  pi;
+		Scanner s1= new Scanner(System.in);
 		
+		 r=s1.nextInt();
+		 pi=s1.nextDouble();
 		
+		Circledk c1 = new Circledk();
+		c1.mul();
 		
-		Scanner s1 = new Scanner(System.in);
-		
-	 int r= s1.nextInt();
-	 double pi=s1.nextDouble();
-		
-		//Circledk c1 = new Circledk();
-		//c1.mul();
-		
-	 double area_of_circle=pi*r*r;
-	 
-	 System.out.println(area_of_circle);
-		
-		
-		
-		
-
+		//double area_of_cirlce=pi*r*r;
+		//System.out.println(area_of_cirlce);
 	}
 
 }
